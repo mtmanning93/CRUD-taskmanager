fd19c819ca7c80c92aa4cdea1533e330327c4b92
Matt Manning <mtmanning93@hotmail.co.uk>
2023-05-31 07:18:10 +0000

    Add new template add_task and functionality to the form within

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     //  sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
+
+    // datepicker initiallization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy", 
+      i18n: {done: "Select"}
+    });
+
+    // select initiallization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+});
+
