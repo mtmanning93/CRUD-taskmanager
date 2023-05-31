44963f889132ab2c6b34d12b4c352ba263e8f748
Matt Manning <mtmanning93@hotmail.co.uk>
2023-05-31 07:42:05 +0000

    CR functionailty given to user when adding tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -13,5 +13,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // select initiallization
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
+
+    // collapsible initiallization
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
 });
 
